e2e8d621b1a5772e4bcd6fedfd0b5b83f5f0dfb5
bharatsingh01102005 <159617707+bharatsingh01102005@users.noreply.github.com>
2025-08-02 22:47:37 +0530

    Create 22-GenerateParentheses

---
+++ 22-GenerateParentheses/22-GenerateParentheses.java
@@ -0,0 +1,16 @@
+// Last updated: 8/2/2025, 10:47:26 PM
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList<>();
+        helper("",n,n,ans);
+        return ans;
+    }
+    void helper(String s,int left,int right,List<String> ans){
+        if(left ==0 && right==0){
+            ans.add(s);
+            return;
+        }
+        if(left>0)helper(s+"(",left - 1,right,ans);
+        if(right>left)helper(s+")",left,right - 1,ans);
+    }
+}
