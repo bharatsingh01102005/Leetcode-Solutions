a60a6f67d5b1318c05435586213b49735aeb6d5f
bharatsingh01102005 <159617707+bharatsingh01102005@users.noreply.github.com>
2025-08-02 22:43:47 +0530

    Create 3656-MinimumNumberOfOperationsToMakeElementsInArrayDistinct

---
+++ 3656-MinimumNumberOfOperationsToMakeElementsInArrayDistinct/3656-MinimumNumberOfOperationsToMakeElementsInArrayDistinct.java
@@ -0,0 +1,16 @@
+// Last updated: 8/2/2025, 10:43:35 PM
+import java.util.*;
+
+class Solution {
+    public int minimumOperations(int[] nums) {
+        int n = nums.length;
+        Set<Integer> seen = new HashSet<>();
+        for (int i = n - 1; i >= 0; i--) {
+            if (seen.contains(nums[i])) {
+                return i / 3 + 1;
+            }
+            seen.add(nums[i]);
+        }
+        return 0;
+    }
+}
