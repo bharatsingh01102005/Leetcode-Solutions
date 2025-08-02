575981cbd2329100ad97fcdf34d52a60c5ad1278
bharatsingh01102005 <159617707+bharatsingh01102005@users.noreply.github.com>
2025-08-02 22:44:41 +0530

    Create 2626-CountTheNumberOfGoodSubarrays

---
+++ 2626-CountTheNumberOfGoodSubarrays/2626-CountTheNumberOfGoodSubarrays.java
@@ -0,0 +1,32 @@
+// Last updated: 8/2/2025, 10:44:29 PM
+class Solution {
+    public long countGood(int[] nums, int k) {
+        
+        Map<Integer, Integer> freq = new HashMap<>();
+        
+        long ans = 0;    
+        long pairs = 0;     
+        int left = 0;        
+
+        for (int right = 0; right < nums.length; right++) {
+            int val = nums[right];
+
+            pairs += freq.getOrDefault(val, 0); 
+     
+            freq.put(val, freq.getOrDefault(val, 0) + 1);
+
+            
+            while (pairs >= k) {
+               
+                ans += nums.length - right;
+
+                
+                int out = nums[left++];
+                freq.put(out, freq.get(out) - 1);   
+                pairs -= freq.get(out);             
+            }
+        }
+
+        return ans;
+    }
+}
