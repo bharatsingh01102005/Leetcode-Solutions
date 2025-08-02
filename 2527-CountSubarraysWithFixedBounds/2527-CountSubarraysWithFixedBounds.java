9b4c0f2056c3effd22bdaed4ce59244de4beeb72
bharatsingh01102005 <159617707+bharatsingh01102005@users.noreply.github.com>
2025-08-02 22:44:44 +0530

    Create 2527-CountSubarraysWithFixedBounds

---
+++ 2527-CountSubarraysWithFixedBounds/2527-CountSubarraysWithFixedBounds.java
@@ -0,0 +1,16 @@
+// Last updated: 8/2/2025, 10:44:32 PM
+class Solution {
+    public long countSubarrays(int[] nums, int minK, int maxK) {
+        long res = 0;
+        int bad = -1, left = -1, right = -1;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < minK || nums[i] > maxK) bad = i;
+            if (nums[i] == minK) left = i;
+            if (nums[i] == maxK) right = i;
+            res += Math.max(0, Math.min(left, right) - bad);
+        }
+
+        return res;
+    }
+}
