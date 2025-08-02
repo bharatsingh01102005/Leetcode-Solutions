55b98e694ac56acc3bd7fbcb0029a6d71a399236
bharatsingh01102005 <159617707+bharatsingh01102005@users.noreply.github.com>
2025-08-02 22:45:28 +0530

    Create 1302-DeleteCharactersToMakeFancyString

---
+++ 1302-DeleteCharactersToMakeFancyString/1302-DeleteCharactersToMakeFancyString.java
@@ -0,0 +1,15 @@
+// Last updated: 8/2/2025, 10:45:16 PM
+class Solution {
+    public String makeFancyString(String s) {
+        StringBuilder answer = new StringBuilder();
+        for(int i = 0; i<s.length(); i++){
+            char c = s.charAt(i);
+            int len = answer.length();
+            if(len<=1 || !(answer.charAt(len-1)==c && answer.charAt(len-2)==c)){
+                answer.append(c);
+            }
+        }
+        return answer.toString();
+        
+    }
+}
