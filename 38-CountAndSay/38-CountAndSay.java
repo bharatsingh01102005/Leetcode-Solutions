83263d353c7c4f1b4ee7825fe90417604f2a10de
bharatsingh01102005 <159617707+bharatsingh01102005@users.noreply.github.com>
2025-08-02 22:47:22 +0530

    Create 38-CountAndSay

---
+++ 38-CountAndSay/38-CountAndSay.java
@@ -0,0 +1,31 @@
+// Last updated: 8/2/2025, 10:47:10 PM
+class Solution {
+    public String countAndSay(int n) {
+        int i = 1;
+        StringBuilder sb = new StringBuilder("1");
+        while(i<n){
+            sb = convert(sb);
+            i++;
+            // sb = s;
+        }
+        return sb.toString();
+    }
+    public StringBuilder convert(StringBuilder sb){
+        StringBuilder s = new StringBuilder();
+        int count = 1;
+        int left = 0;
+        int right = 1;
+        while(left <sb.length()){
+            while(right<sb.length() && sb.charAt(left)==sb.charAt(right)){
+                right++;
+            
+                count++;
+            }
+            s.append(count+""+sb.charAt(left));
+            left = right;
+            right++;
+            count = 1;
+        }
+        return s;
+    }
+}
