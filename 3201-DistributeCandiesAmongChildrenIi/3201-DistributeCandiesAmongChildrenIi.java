05643c0e9d00a20260bf47b2819e012b43fd0b92
bharatsingh01102005 <159617707+bharatsingh01102005@users.noreply.github.com>
2025-08-02 22:44:22 +0530

    Create 3201-DistributeCandiesAmongChildrenIi

---
+++ 3201-DistributeCandiesAmongChildrenIi/3201-DistributeCandiesAmongChildrenIi.java
@@ -0,0 +1,20 @@
+// Last updated: 8/2/2025, 10:44:10 PM
+class Solution {
+    public long distributeCandies(int n, int limit) {
+        int min = Math.max(0, n - 2 * limit); // Minimum candies first child can get
+        int max = Math.min(n, limit);         // Maximum candies first child can get
+        long ways = 0;
+
+        for (int i = min; i <= max; i++) {
+            int N = n - i; // Remaining candies after giving i to the first child
+
+            // Calculate valid range for second child
+            int minCh2 = Math.max(0, N - limit);
+            int maxCh2 = Math.min(N, limit);
+
+            ways += maxCh2 - minCh2 + 1;
+        }
+
+        return ways;
+    }
+}
