1d3585580800c29cc3e4965b56e0bbb265b9c40d
bharatsingh01102005 <159617707+bharatsingh01102005@users.noreply.github.com>
2025-08-02 22:46:53 +0530

    Create 69-Sqrtx

---
+++ 69-Sqrtx/69-Sqrtx.java
@@ -0,0 +1,31 @@
+// Last updated: 8/2/2025, 10:46:41 PM
+class Solution {
+    public int mySqrt(int x) {
+        // Corner cases.
+        if (x == 0 || x == 1) return x;
+
+        // Find the number which square less than x.
+        int y = x;
+        while (true) {
+            y = y / 2;
+            if ((y * y) < 0 || (y * y) / y != y) continue; // handle int overflow
+            if ((y * y) > 0 && (y * y) <= x) break;
+        }
+
+        // Result found.
+        if (y * y == x) return y;
+
+        // Search for result using difference of squares formula.
+        int j = x - (y * y);
+        while (true) {
+            j = j - (y + y + 1);
+            if (j >= 0) {
+                y = y + 1;
+            } else {
+                break;
+            }
+        }
+
+        return y;
+    }
+}
