fe850d46213ed628f5432ed8c2eec6944647adcb
bharatsingh01102005 <159617707+bharatsingh01102005@users.noreply.github.com>
2025-08-02 22:47:41 +0530

    Create 19-RemoveNthNodeFromEndOfList

---
+++ 19-RemoveNthNodeFromEndOfList/19-RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,30 @@
+// Last updated: 8/2/2025, 10:47:29 PM
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode slow=head;
+        ListNode fast=head;
+        ListNode prev=null;
+  
+       for(int i=1;i<=n;i++){
+        fast=fast.next;
+       }
+        if(fast==null) return head.next;
+        while(fast!=null){
+            prev=slow;
+            fast=fast.next;
+            slow=slow.next;
+        }
+        prev.next=slow.next;
+        return head;
+    }
+}
